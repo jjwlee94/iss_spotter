7b4a21c32f50a0b44c18512bf9334af53ccf5db7
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-04 17:07:36 +0000

    Fetch coordinates

--- iss.js
+++ iss.js
@@ -8,18 +8,34 @@
  */
 const request  = require("request");
 
-const fetchMyIP = function(callback) {
-// use request to fetch IP address from JSON API
-  request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) {
-      return callback(error, null);
-    }
-    if (response.statusCode !== 200) {
-      callback(Error(`Status code ${response.statusCode} when fetching IP: ${body}`), null);
-    }
-    const ip = JSON.parse(body).ip;
-    callback(null, ip);
-  });
-};
+// const fetchMyIP = function(callback) {
+// // use request to fetch IP address from JSON API
+//   request('https://api.ipify.org?format=json', (error, response, body) => {
+//     if (error) {
+//       return callback(error, null);
+//     }
+//     if (response.statusCode !== 200) {
+//       callback(Error(`Status code ${response.statusCode} when fetching IP: ${body}`), null);
+//     }
+//     const ip = JSON.parse(body).ip;
+//     callback(null, ip);
+//   });
+// };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  // use request to fetch IP address from JSON API
+    request(`https://freegeoip.app/json/?apikey=${ip}`, (error, response, body) => {
+      if (error) {
+        return callback(error, null);
+      }
+      if (response.statusCode !== 200) {
+        callback(Error(`Status code ${response.statusCode} when fetching IP: ${body}`), null);
+      }
+      const latitude = JSON.parse(body).latitude;
+      const longitude = JSON.parse(body).longitude;
+      const coordinates = {latitude, longitude}
+      callback(null, coordinates);
+    });
+  };
+
+module.exports = { fetchCoordsByIP };
