15021c6dd9cd7131c492cf27743f1e5c74a89ed1
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-04 13:57:49 +0000

    Fetch IP

--- iss.js
+++ iss.js
@@ -6,8 +6,20 @@
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
- const fetchMyIP = function(callback) { 
-  // use request to fetch IP address from JSON API
-}
+const request  = require("request");
+
+const fetchMyIP = function(callback) {
+// use request to fetch IP address from JSON API
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status code ${response.statusCode} when fetching IP: ${body}`), null);
+    }
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
 
 module.exports = { fetchMyIP };
