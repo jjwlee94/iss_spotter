f89b8fe764995c0f3c9de0b47c72f761cb3ac357
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-04 19:21:33 +0000

    Fetch ISS flyover times

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+// 
+// const ip = function(callback) {
+//   // use request to fetch IP address from JSON API
+//     request('https://api.ipify.org?format=json', (error, response, body) => {
+//       if (error) {
+//         return callback(error, null);
+//       }
+//       if (response.statusCode !== 200) {
+//         callback(Error(`Status code ${response.statusCode} when fetching IP: ${body}`), null);
+//       }
+//       const ip = JSON.parse(body).ip;
+//       callback(null, ip);
+//     });
+//   };
+// 
+// fetchCoordsByIP(ip, (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+// 
+//   console.log('It worked! Returned coordinates:', coordinates);
+// 
+// });
 
-const ip = function(callback) {
-  // use request to fetch IP address from JSON API
-    request('https://api.ipify.org?format=json', (error, response, body) => {
-      if (error) {
-        return callback(error, null);
-      }
-      if (response.statusCode !== 200) {
-        callback(Error(`Status code ${response.statusCode} when fetching IP: ${body}`), null);
-      }
-      const ip = JSON.parse(body).ip;
-      callback(null, ip);
-    });
-  };
+const exampleCoords = { latitude: '43.6667', longitude: '-79.3804' };
 
-fetchCoordsByIP(ip, (error, coordinates) => {
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
-
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
